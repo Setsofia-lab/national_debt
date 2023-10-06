25135de5fae583b678924fe03450b497e6859794
Samset <setsofiaeli@gmail.com>
2023-10-06 15:17:25 +0000

    spider working

--- national_debt/spiders/nationalgdp.py
+++ national_debt/spiders/nationalgdp.py
@@ -7,10 +7,10 @@ class NationalgdpSpider(scrapy.Spider):
     start_urls = ['http://worldpopulationreview.com/country-rankings/countries-by-national-debt/']
 
     def parse(self, response):
-        nationgdp = response.xpath("//table/tr/td[2]")
+        nationgdp = response.xpath("//table/tr")
         for rank in nationgdp:
-            nation = rank.xpath(".//td[2]/text()").get()
-            gdp = rank.xpath(".//td[3]/text()").get()
+            nation = rank.xpath(".//td[6]").get()
+            gdp = rank.xpath(".//td[7]/text()").get()
 
             yield{
                 'nation': nation,
